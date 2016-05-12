0db811ebb872e673c82ac5d916db3ea35f01e9fe
Buchori W Hidayat <buchori.12ipa6@gmail.com>
2016-05-13 05:43:15 +0700

    Jawaban Test No 3

--- FunctionAPI.java
+++ FunctionAPI.java
@@ -5,9 +5,12 @@ import java.util.TreeMap;
 
 public class FunctionAPI {
 
-	//using treemap, so it will order ascending and for better latency purpose
-	//Source : 
-	//http://stackoverflow.com/questions/7057430/treemap-or-hashmap-faster
+	/*
+	I will using treemap for store data, so it will order ascending and for better latency purpose
+	Source : 
+	http://stackoverflow.com/questions/7057430/treemap-or-hashmap-faster
+	*/
+	
 	static Map<Integer, String> blackList = new TreeMap<Integer, String>();
 	
 	public static void main(String[] args) {
@@ -24,40 +27,34 @@ public class FunctionAPI {
 	public static void initialize(String blacklist){
 		
 		// reference one line at a time
-        String line = null;
+        	String line = null;
 
-        try {
-            // FileReader reads text files
-            FileReader fileReader = new FileReader(blacklist);
+        	try {
+            		// FileReader reads text files
+            		FileReader fileReader = new FileReader(blacklist);
 
-            // wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            		// wrap FileReader in BufferedReader.
+            		BufferedReader bufferedReader = new BufferedReader(fileReader);
             
-            //declare temp variable
-            String[] split = new String[2];
+            		//declare temp variable
+            		String[] split = new String[2];
             
-            //input data to treeMap
-            while((line = bufferedReader.readLine()) != null) {
-            	split = line.split(" ");
-            	blackList.put(Integer.parseInt(split[1]), split[0]);
-            }   
+            		//input data to treeMap
+            		while((line = bufferedReader.readLine()) != null) {
+            			split = line.split(" ");
+            			blackList.put(Integer.parseInt(split[1]), split[0]);
+            		}   
             
-            // close files.
-            bufferedReader.close();  
-            
-          //for testing purpose
-//            for (Map.Entry<Integer, String> entry : blackList.entrySet())
-//            {
-//                System.out.println(entry.getKey() + "-/-" + entry.getValue());
-//            }
-            
-        }
-        catch(FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + blacklist + "'");                
-        }
-        catch(IOException ex) {
-        	ex.printStackTrace();
-        }
+            		// close files.
+            		bufferedReader.close();  
+          
+        	}
+        	catch(FileNotFoundException ex) {
+            		System.out.println("Unable to open file '" + blacklist + "'");                
+        	}
+        	catch(IOException ex) {
+        		ex.printStackTrace();
+        	}
 	}
 	
 	//check_blacklist(name, phone_number)
@@ -65,8 +62,8 @@ public class FunctionAPI {
 	//This function is called whenever the API is called, 
 	//and return boolean the input name and phone number is in the blacklist.
 	public static Boolean check_blacklist(String name, int phone_number){
-
+		
 		return blackList.containsKey(phone_number) && blackList.containsValue(name);
-
+		//or we can using Search Algorithm for Sorted Data if this method doesn't enough fast for each API Call
 	}
 }
